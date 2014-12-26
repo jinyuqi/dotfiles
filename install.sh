4e217f0200d7d6fb0ffe313528f7ee254254945f
jinyuqi <jinyuqi@softlinuxserver-Z68P-DS3>
2014-12-26 16:50:18 +0800

    change commit

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Description: dotfiles's install - create each symbolic link in $HOME directory
 
 #!/bin/bash
-echo "Link symbolic..."
+echo "Linking symbolic..."
 ln -s ${PWD}/.gdbinit ${HOME}/.gdbinit -f && echo ".gdbinit done"
 ln -s ${PWD}/.gitconfig ${HOME}/.gitconfig -f && echo ".gitconfig done"
 ln -s ${PWD}/.vimrc ${HOME}/.vimrc -f && echo ".vimrc done"
