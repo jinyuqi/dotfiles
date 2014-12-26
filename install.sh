be2ad970b88e2577a5bcdaa5df1bc580cc68d39c
jinyuqi <jinyuqi@softlinuxserver-Z68P-DS3>
2014-12-26 16:24:21 +0800

    add install file

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Author: Jin Yuqi
+# Description: dotfiles's install - create each symbolic link in $HOME directory
+
+#!/bin/bash
+ln -s ./.gdbinit ~/.gdbinit -f
+ln -s ./.gitconfig ~/.gitconfig -f
+ln -s ./.vimrc ~/.vimrc -f
+ln -s ./.vimperatorrc ~/.vimperatorrc -f
