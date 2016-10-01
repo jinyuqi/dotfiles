155f3efc939c8b1abcd4995afb74a0292e547ea5
Jin Yuqi <179386223@qq.com>
2016-10-01 00:53:59 -0700

    symbol link vimrc

--- prepare_vim.sh
+++ prepare_vim.sh
@@ -1,6 +1,8 @@
 # Author: Jin Yuqi
 # Email: 179386223@qq.com
 
+ln -s $PWD/vimrc $HOME/.vimrc
+
 git clone http://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim +PluginInstall +qall && mkdir -p ~/.vim/colors && cp ~/.vim/bundle/molokai/colors/molokai.vim ~/.vim/colors 
