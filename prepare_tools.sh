25b6a5804c0bd1815a35526278e7908e9cd5c940
Jin Yuqi <179386223@qq.com>
2016-10-24 21:00:38 +0800

    add prepare_tools.sh

---
+++ prepare_tools.sh
@@ -0,0 +1,9 @@
+# Author: Jin Yuqi
+# Email: 179386223@qq.com
+
+sudo apt-get install vim-gnome
+sudo apt-get install cmake
+sudo apt-get install git
+sudo apt-get install ctags
+sudo apt-get install meld
+sudo apt-get install tig
