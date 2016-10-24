c20f1cc533d7d74fc17ec6bb0e5444b56673162a
Jin Yuqi <179386223@qq.com>
2016-10-24 21:00:05 +0800

    add font set for vim_prepare

--- prepare_vim.sh
+++ prepare_vim.sh
@@ -1,6 +1,8 @@
 # Author: Jin Yuqi
 # Email: 179386223@qq.com
 
+mkdir -p ~/.fonts && cp monaco.ttf ~/.fonts && fc-cache -v
+
 ln -s $PWD/vimrc $HOME/.vimrc
 
 git clone http://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
